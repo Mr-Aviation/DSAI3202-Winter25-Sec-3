7f18b57c7f827c595817175013945e620036a069
60300359 <60300359@udst.edu.qa>
2025-04-12 03:31:47 +0300

    " "

---
+++ Assignment2/tasks.py
@@ -0,0 +1,21 @@
+
+from celery import Celery
+from src.maze import create_maze
+from src.explorer import Explorer
+
+app = Celery('maze_tasks', broker='pyamqp://guest@localhost//', backend='rpc://')
+
+@app.task
+def run_explorer_task(index, maze_type, width, height):
+    
+    maze = create_maze(width, height, maze_type)
+    explorer = Explorer(maze, visualize=False)
+    time_taken, moves = explorer.solve()
+    
+    return {
+        "id": index,
+        "time": time_taken,
+        "moves": len(moves),
+        "backtracks": explorer.backtrack_count
+    }
+
